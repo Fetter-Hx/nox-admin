6f3993321e579b07305c3d7c3d5a8bf09e580f04
HX <1135224820@qq.com>
2024-09-14 15:36:26 +0800

    chore: add husky and lint-staged for configuration

---
+++ lint-staged.config.mjs
@@ -0,0 +1,5 @@
+export default {
+  '*.vue': ['prettier --write', 'eslint --cache --fix'],
+  '*.{js,ts}': ['prettier --cache --ignore-unknown --write', 'eslint --cache --fix'],
+  'package.json': ['prettier --cache --write', 'eslint --cache --fix']
+}
