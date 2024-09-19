0dde4d4961c73d452a6661d336dcc557829eeeaa
HX <1135224820@qq.com>
2024-09-19 15:32:40 +0800

    chore: add style checks in lint-staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,9 @@
 export default {
   '*.vue': ['prettier --write', 'eslint --cache --fix'],
   '*.{js,ts}': ['prettier --cache --ignore-unknown --write', 'eslint --cache --fix'],
+  '*.{scss,html,vue,css}': [
+    'prettier --cache --ignore-unknown',
+    'stylelint --fix --allow-empty-input'
+  ],
   'package.json': ['prettier --cache --write']
 }
