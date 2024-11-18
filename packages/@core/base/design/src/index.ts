330fc086303baf20c62f1b99d00c06b750dcec2d
HX <hanxu0410@gmail.com>
2024-11-18 14:09:00 +0800

    feat(@nox-core/design): :sparkles: css配置包以及design-token的配置

---
+++ packages/@core/base/design/src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: HX
+ * @Date: 2024-10-31 16:21:25
+ * @LastEditors: HX
+ * @LastEditTime: 2024-11-14 10:12:20
+ * @FilePath: \nox-monorepo\packages\@core\base\design\src\index.ts
+ * @Description:
+ *
+ */
+import './design-tokens'
+import './css/global.css'
+import './css/transition.css'
