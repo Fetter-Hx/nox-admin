0ad3e645a1d3d8bfcbea16903024b8213cc3ee0f
HX <hanxu0410@gmail.com>
2025-03-17 16:27:18 +0800

    feat(@nox/constants): :sparkles: app公用常量包

---
+++ packages/constants/README.md
@@ -0,0 +1,3 @@
+# @nox/constants
+
+app公用的常量，继承@nox-core/shared/constants的所有能力，业务上通用的常量存放在这里
---
+++ packages/constants/src/core.ts
@@ -0,0 +1,5 @@
+/** 默认首页url */
+export const DEFAULT_HOME_PATH = '/analytics'
+
+/** 默认登录url */
+export const LOGIN_PATH = '/auth/login'
